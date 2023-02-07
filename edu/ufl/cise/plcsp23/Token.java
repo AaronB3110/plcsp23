7f696e4116a55cc6191c1eab45a90d7852d288fd
AaronB3110 <aarondavidb31@gmail.com>
2023-02-06 20:16:18 -0500

    Starting the token file

---
+++ edu/ufl/cise/plcsp23/Token.java
@@ -0,0 +1,34 @@
+package edu.ufl.cise.plcsp23;
+
+public class Token implements IToken {
+    final Kind kind;
+    final String input;
+    final int line;
+    final int column;
+    SourceLocation loc;
+
+    public Token(Kind kind, String input, int line, int column){
+        this.kind = kind;
+        this.input = input;
+        this.line = line;
+        this.column = column;
+        this.loc = new SourceLocation(line, column);
+    }
+
+    @Override
+    public SourceLocation getSourceLocation() {
+        return loc;
+    }
+
+    @Override
+    public Kind getKind() {
+        return kind;
+    }
+
+    @Override
+    public String getTokenString() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
